4e026bee1f39b08ba782da0b61b610210ace2b71
vincentmrlau <413893093@qq.com>
2017-08-12 11:38:32 +0800

    remove-demo

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
  * Created by liuyiman on 2017/8/10.
  */
 
-import ImageViewer from './ImageViewerExample/SingleImageZoomViewer'
+import ImageViewer from './src/SingleImageZoomViewer'
 
 export default ImageViewer
